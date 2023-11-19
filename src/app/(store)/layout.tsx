e3ace35c3b3ee85501fdb7fd494e93f6bf659e5a
Guilherme V. <imluizguilara@hotmail.com>
2023-11-19 17:11:45 -0300

    feat: Setup Header component

---
+++ src/app/(store)/layout.tsx
@@ -0,0 +1,15 @@
+import { Header } from '@/components/header'
+import { ReactNode } from 'react'
+
+type StoreLayoutProps = {
+	children: ReactNode
+}
+
+export default function StoreLayout({ children }: StoreLayoutProps) {
+	return (
+		<div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
+			<Header />
+			{children}
+		</div>
+	)
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 			lang="pt-BR"
 			className={inter.variable}
 		>
-			<body>{children}</body>
+			<body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
 		</html>
 	)
 }
