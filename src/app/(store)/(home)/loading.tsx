f1b5231b5db9516e033eb7ae7ec5124799094bd2
Guilherme V. <imluizguilara@hotmail.com>
2023-11-20 12:19:00 -0300

    feat: Add Cache && Skeleton screen on Home

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,3 +1,11 @@
-export default function Loading() {
-	return <div>loading</div>
+import { Skeleton } from "@/components/skeleton";
+
+export default function HomeLoading() {
+  return (
+    <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
+      <Skeleton className="col-span-6 row-span-6 h-[855px]" />
+      <Skeleton className="col-span-3 row-span-3" />
+      <Skeleton className="col-span-3 row-span-3" />
+    </div>
+  );
 }
--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -2,6 +2,8 @@ import data from "../data.json";
 
 // Quando essa chamada for realizada com o método GET
 export async function GET() {
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // Artificial delay for dev. purposes
+
   const featuredProducts = data.products.filter((product) => product.featured);
 
   return Response.json(featuredProducts);
